2d8b014ce75996e35e6c36b3f3364825400b3fb0
Tom Li Dobnik <tomlidobnik1@gmail.com>
2025-04-29 16:04:52 +0200

    feat: povezovanje z odjemalcem (SCRUM-102)

--- backend/src/routers/root.rs
+++ backend/src/routers/root.rs
@@ -1,27 +1,45 @@
-use crate::routers::user_router;
-use axum::Router;
+use crate::handlers::web_socket_handler::{handle_ws, Connections};
+use crate::routers::{event_router, location_router, user_router};
+use axum::{Router, routing::get};
 use log::info;
 use pretty_env_logger;
 use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use tower_http::cors::{Any, CorsLayer};
 
-use super::{event_router, location_router};
-
-pub async fn create_router() -> Router {
+pub async fn create_router(connections: Connections) -> Router {
     let cors = CorsLayer::new().allow_origin(Any).allow_methods(Any);
+
+    let ws_routes = Router::new().route("/", get({
+        let connections = connections.clone();
+        move |ws, connect_info| handle_ws(ws, connect_info, connections.clone())
+    }));
+
     let api_routes = Router::new()
         .nest("/user", user_router::create_router())
         .nest("/event", event_router::create_router())
         .nest("/location", location_router::create_router());
-    Router::new().nest("/api", api_routes).layer(cors)
+
+    Router::new()
+        .nest("/ws", ws_routes)
+        .nest("/api", api_routes)
+        .layer(cors)
 }
 
 pub async fn run_server() {
     pretty_env_logger::init();
     let addr: SocketAddr = "0.0.0.0:8000".parse().unwrap();
     info!("Server running at http://{}", addr);
-    let app = create_router().await;
-    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
-        .await
-        .unwrap();
+
+    let connections: Connections = Arc::new(Mutex::new(HashMap::new()));
+    let app = create_router(connections).await;
+
+    axum::serve(
+        tokio::net::TcpListener::bind(addr).await.unwrap(),
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
