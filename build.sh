617a690381359f5ccf530017664a96ef55468ea7
Tom Li Dobnik <tomlidobnik1@gmail.com>
2025-05-04 23:27:34 +0200

    feat: optimizacija grajenja aplikacije (SCRUM-132)

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@ echo "$BACKEND_ENV_FILE" > ./backend/.env
 echo "🔨 Building and pushing backend for linux/amd64..."
 docker buildx build --platform linux/amd64 -t tomlidobnik/backend:prod -f backend/Dockerfile ./backend --push
 
-echo "$FRONTEND_ENV_FILE" > ./frontend/.env
-
 echo "🔨 Building and pushing frontend for linux/amd64..."
 docker buildx build --platform linux/amd64 -t tomlidobnik/frontend:prod -f frontend/Dockerfile ./frontend --push
 
