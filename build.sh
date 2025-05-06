b1a5ec22d5189088c971fb58646645abae3a4478
Tom Li Dobnik <tomlidobnik1@gmail.com>
2025-05-06 20:25:40 +0200

    update: azure -> pi

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ set -e
 
 docker buildx create --use --name multi-arch-builder || docker buildx use multi-arch-builder
 
-echo "🔨 Building and pushing backend for linux/amd64..."
-docker buildx build --platform linux/amd64 -t tomlidobnik/backend:prod -f backend/Dockerfile ./backend --push
+echo "🔨 Building and pushing backend for linux/arm64..."
+docker buildx build --platform linux/arm64 -t tomlidobnik/backend:prod -f backend/Dockerfile ./backend --push
 
-echo "🔨 Building and pushing frontend for linux/amd64..."
-docker buildx build --platform linux/amd64 -t tomlidobnik/frontend:prod -f frontend/Dockerfile ./frontend --push
+echo "🔨 Building and pushing frontend for linux/arm64..."
+docker buildx build --platform linux/arm64 -t tomlidobnik/frontend:prod -f frontend/Dockerfile ./frontend --push
 
 echo "✅ Build and push complete!"
 
