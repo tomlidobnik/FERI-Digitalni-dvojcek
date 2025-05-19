d9fcdd51a77184c0027c7ec7b9c2273c4d0470a1
Tom Li Dobnik <tomlidobnik1@gmail.com>
2025-05-19 10:00:17 +0200

    feat: nalaganje lokacij (SCRUM-162)

--- backend/src/routers/event_router.rs
+++ backend/src/routers/event_router.rs
@@ -10,4 +10,7 @@ pub fn create_router() -> Router {
         .route("/by_id/{id}", get(get_event_by_id))
         .route("/delete/{id}", delete(delete_event))
         .route("/available", get(get_available_events))
+        .route("/make_public/{id}", put(make_event_public))
+        .route("/make_private/{id}", put(make_event_private))
+        .route("/allow_user/{id}", post(add_event_allowed_user))
 }
