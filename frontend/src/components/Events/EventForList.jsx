1bcde6e16c0537c07b8e25ecb9853794f9e2b09e
anejbr <anej.bregant@gmail.com>
2025-05-25 16:17:32 +0200

    fix: Date format

--- frontend/src/components/Events/EventForList.jsx
+++ frontend/src/components/Events/EventForList.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 
 const formatDateTime = (dateString) => {
+    const currentDate = new Date();
     const date = new Date(dateString);
+    if (currentDate.getDay() === date.getDay() &&
+        currentDate.getMonth() === date.getMonth() &&
+        currentDate.getFullYear() === date.getFullYear()) {
+        return `Danes ob ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+    };
     return `${date.getDate()}. ${date.getMonth() + 1}. ${date.getFullYear()} ob ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
 };
 
--- frontend/src/components/Events/EventForListDetail.jsx
+++ frontend/src/components/Events/EventForListDetail.jsx
@@ -8,7 +8,13 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const formatDateTime = (dateString) => {
+    const currentDate = new Date();
     const date = new Date(dateString);
+    if (currentDate.getDay() === date.getDay() &&
+        currentDate.getMonth() === date.getMonth() &&
+        currentDate.getFullYear() === date.getFullYear()) {
+        return `Danes ob ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+    };
     return `${date.getDate()}. ${date.getMonth() + 1}. ${date.getFullYear()} ob ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
 };
 
