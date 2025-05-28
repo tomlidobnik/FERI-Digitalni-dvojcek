75c19103eedeeace8de4b28aa4247ee2ee11efc0
Alen Kolmanic <157404999+alenk2@users.noreply.github.com>
2025-05-28 21:22:22 +0200

    feat: Implementacija parserja, scannerja, GeoJSON pretvorba, AST in DFA (SCRUM-204)

--- PPJ/src/Main.kt
+++ PPJ/src/Main.kt
@@ -1,26 +1,4 @@
 import java.io.File
-//
-//fun main() {
-//    val input = File("src/examples.txt").readText()
-//    val scanner = Scanner(input)
-//
-//    println("Tokens:")
-//    while (true) {
-//        val token = scanner.nextToken()
-//        println(token)
-//        if (token.type == TokenType.EOF) break
-//    }
-//
-//    val parser = Parser(Scanner(input))
-//
-//    try {
-//        parser.parseExpr()
-//        println("Parsing successful.")
-//    } catch (e: Exception) {
-//        println("Parsing failed: ${e.message}")
-//    }
-//}
-
 
 fun main() {
     val input = File("src/examples.txt").readText()
